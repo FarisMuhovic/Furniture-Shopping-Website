aeeb067ed4e2848221359d556ce4a71ec9a8562d
FarisMuhovic <faris.fare123456@gmail.com>
2022-10-20 00:23:49 +0200

    responsive / hamburger

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const aboutNav = document.getElementById("about");
+const hamburgerBtn = document.getElementById("hamburger");
+const hamburgerExit = document.getElementById("hamburger-exit");
+const mainNavs = document.getElementById("main-navigation");
+hamburgerBtn.addEventListener("click", () => {
+  mainNavs.classList.add("mobile-main-navs");
+  hamburgerExit.classList.add("mobile-exit-btn");
+});
+hamburgerExit.addEventListener("click", () => {
+  mainNavs.classList.remove("mobile-main-navs");
+  hamburgerExit.classList.remove("mobile-exit-btn");
+});
