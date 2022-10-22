034f9aa1a4b57003270c23c063181b8ae8f6d741
FarisMuhovic <faris.fare123456@gmail.com>
2022-10-22 13:14:22 +0200

    carousel items

--- script.js
+++ script.js
@@ -9,3 +9,53 @@ hamburgerExit.addEventListener("click", () => {
   mainNavs.classList.remove("mobile-main-navs");
   hamburgerExit.classList.remove("mobile-exit-btn");
 });
+
+const rightBtn = document.getElementById("right-btn");
+const leftBtn = document.getElementById("left-btn");
+const items = document.querySelectorAll(".item");
+
+let translateVar = 0;
+let translateValue = 0;
+if (window.innerWidth < 600) {
+  translateValue += 335;
+} else {
+  translateValue += 533;
+}
+const itemGap = 10;
+let countRight = 0;
+
+rightBtn.addEventListener("click", () => {
+  if (translateVar > -translateValue * 6 && countRight < 6) {
+    countRight++;
+    translateVar -= translateValue;
+    document.documentElement.style.setProperty(
+      "--itemtranslate",
+      translateVar + "px"
+    );
+  } else {
+    translateVar = 0;
+    countRight = 0;
+    document.documentElement.style.setProperty(
+      "--itemtranslate",
+      translateVar + "px"
+    );
+  }
+});
+leftBtn.addEventListener("click", () => {
+  if (translateVar < 0) {
+    countRight--;
+    translateVar += translateValue;
+    document.documentElement.style.setProperty(
+      "--itemtranslate",
+      translateVar + "px"
+    );
+  } else {
+    translateVar = -translateValue * 6 + itemGap;
+    countRight = 6;
+    document.documentElement.style.setProperty(
+      "--itemtranslate",
+      translateVar + "px"
+    );
+  }
+});
+// change instead of pixels to percentage for more responsivness
